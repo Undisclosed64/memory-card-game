204a6c8332956d91f2072e732b1a20934715a0c5
Undisclosed <undisclosed674@gmail.com>
2021-11-05 08:04:53 -0700

    commit changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,18 @@
 
 import {useState} from 'react';
 import '../App.css';
+import AnneBoonchuy from '../images/anne.jpg';
+import SadieCroaker from '../images/croaker.png';
+import CaptainGrime from '../images/grime.png';
+import HopPopPlantar from '../images/hoppop.png';
+import LeopoldLoggle from '../images/loggle.png';
+import MaddieFlour from '../images/maddie.jpg';
+import PollyPlantar from '../images/polly.png';
+import Sasha from '../images/sasha.jpg';
+import SprigPlantar from '../images/sprig.png';
+import SylviaSundew from '../images/sylvia.png';
+import MayorToadstool from '../images/toadstool.jpg';
+import Wally from '../images/wally.png';
 
 const initialState = {
     score: 0,
@@ -13,16 +25,30 @@ function Cards(){
     const[clickedTimes,setClickedTimes] = useState(0)
     const [highestScore,sethighestScore] = useState(0);
     const [scoreArr,setScoreArr] = useState([]);
-    const [cardsArr,setCardsArr] =  useState([{"name":"card1","id":"0"},{"name":"card2","id":"1"},{"name":"card3","id":"2"},{"name":"card4","id":"3"},{"name":"card5","id":"4"},{"name":"card6","id":"5"},{"name":"card7","id":"6"},{"name":"card8","id":"7"}
+    const [cardsArr,setCardsArr] =  useState([
+        {"name":"Anne Boonchuy","source":AnneBoonchuy,"id":"0"},
+        {"name":"Sadie Croaker","source":SadieCroaker,"id":"1"},
+        {"name":"Captain Grime","source":CaptainGrime,"id":"2"},
+        {"name":"Hop Pop Plantar","source":HopPopPlantar,"id":"3"},
+        {"name":"Leopold Loggle","source":LeopoldLoggle,"id":"4"},
+        {"name":"Maddie Flour","source":MaddieFlour,"id":"5"},
+        {"name":"Polly Plantar","source":PollyPlantar,"id":"6"},
+        {"name":"Sasha","source":Sasha,"id":"7"},
+        {"name":"Sprig Plantar","source":SprigPlantar,"id":"8"},
+        {"name":"Sylvia Sundew","source":SylviaSundew,"id":"9"},
+        {"name":"Mayor Toadstool","source":MayorToadstool ,"id":"10"},
+        {"name":"Wally","source":Wally,"id":"11"},
+
+
 ]);
 const [ids,setIds] = useState([]);
 
 const handleClick = (e) => {
 setClickedTimes(clickedTimes+1);
-//console.log(clickedTimes);
 const clickedTarget = e.target;
-checkDoubleClick(clickedTarget.id)
+checkDoubleClick(clickedTarget.id);
 getID(clickedTarget);
+
 
 incrementScore();
 
@@ -33,15 +59,20 @@ incrementScore();
 
 function shuffleArray(array) {
     return array.sort( ()=>Math.random()-0.5 ); 
-}
 
 }
+}
+
 
 function incrementScore(){
+    if(checkDoubleClick === true){
+        console.log('no increase in score');
+    } else {
     setState({score:score+1});
 }
+}
+
 //const clearState = () => {
-   //setState({...initialState});
   // setState(initialState)
    
    // }; 
@@ -57,31 +88,42 @@ function storeId(id){
 function checkDoubleClick(cardId){
     if(ids.indexOf(cardId) !== -1){ 
  clearState();
-console.log("it is present")
+ //testFunction();works fine
+console.log("it is present");
+return true;
+
     } else {
+ //testFunction();
 console.log("it is absent!");
-incrementScore();
+return false;
     }
-    
     }
 const clearState = () => {
-   setState(initialState)
+    setState(initialState);
+
     }; 
 
+const testFunction = () => {
+    console.log('hello')
+}
 
     return(
-        <div>
-         <h3>score:{score}</h3>
-         <h4>Highest score:{highestScore}</h4>
-         <h1 onClick={clearState}>Reset</h1>
-
-         
+        <div id="main">
+         <h3>Score:{score}</h3>
+         <h4>Highest Score:{highestScore}</h4>
+    
+         <div id="cardSection">
         {cardsArr.map
         (function(element,index){
-           return (<button key={index}id={element.id}onClick={handleClick}>{element.name}</button>)
+           return (
+               <div id="card-cell">
+           <img src={element.source}alt=""key={index}id={element.id}onClick={handleClick}></img>
+           <div className="cardName">{element.name}</div>
+           </div>
+         )
           
          })}
-         
+         </div>
          </div>
     ) 
 
